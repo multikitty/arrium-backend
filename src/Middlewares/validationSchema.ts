1274622b875eb745776b7540ff9a8a915f30145b
multikitty <multikitty422@gmail.com>
2022-06-06 15:26:59 +0530

    updated validation schema

--- src/Middlewares/validationSchema.ts
+++ src/Middlewares/validationSchema.ts
@@ -5,17 +5,18 @@ export const validationSchema = (request: any, response: any, next: any) => {
   const errors = validationResult(request);
   if (!errors.isEmpty()) {
     //manupulate errors validation result
-    let errorData: any = [];
+    let errorData: any = {};
     for (let index = 0; index < errors.array().length; index++) {
-
       delete errors.array()[index].value;
       delete errors.array()[index].location;
-      
+
       if (index === 0) {
-        errorData.push(errors.array()[index]);
+        const key = errors.array()[index].param;
+        errorData[key] = errors.array()[index].msg;
       } else {
         if (errors.array()[index].param !== errors.array()[index - 1].param) {
-          errorData.push(errors.array()[index]);
+          const key = errors.array()[index].param;
+          errorData[key] = errors.array()[index].msg;
         }
       }
     }
