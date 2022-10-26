9678c2a1c5c26baf6ae142ba8af027970dfafd74
multikitty <multikitty422@gmail.com>
2022-10-26 18:59:02 +0500

    update

--- src/Controllers/StripeController.ts
+++ src/Controllers/StripeController.ts
@@ -1,8 +1,15 @@
 import { StripeServices } from '../Services/StripeServices';
 export const StripeController = {
   getPricingPlans: async (req: any, res: any) => {
+    const {limit=20,search,active=true,getAll=false}=req.query
+    let query=''
     try {
-      const data = await StripeServices.getPricingPlans();
+      if(getAll){
+        query=''
+      }else if(search){
+        query=`active:${active}`
+      }
+      const data = await StripeServices.getPricingPlans(query);
       return res.status(200).json({ data, error: false, message: 'Successfully fetched pricing plans' });
     } catch (err: any) {
       return res.status(500).json({ error: true, message: err?.message });
--- src/Services/StripeServices.ts
+++ src/Services/StripeServices.ts
@@ -2,9 +2,10 @@ const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
 export const StripeServices = {
-  getPricingPlans: async () => {
+  getPricingPlans: async (query:any) => {
     const products = await stripe.products.search({
-      query: "active:'true' AND name:'(UK)'",
+      query: query,
+      
     });
     return products;
   },
