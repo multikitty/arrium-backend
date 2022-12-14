ab34f5a44f2c319d3a5e911dfc4fef178114cd9c
multikitty <multikitty422@gmail.com>
2022-12-14 17:31:10 +0530

    merged zendesk code

--- src/Controllers/ZendeskController.ts
+++ src/Controllers/ZendeskController.ts
@@ -1,31 +1,30 @@
 import ZendeskServices from "../Services/ZendeskServices";
-import { v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import { ZendeskCreateTicket } from "../Interfaces/zendeskInterface";
 
 export default class ZendeskController {
 
- // upload file controller
-  async uploadFile(request:any, response:any){
+  // upload file controller
+  async uploadFile(request: any, response: any) {
     const fileName = uuidv4()
     fs.writeFileSync(`uploads/${fileName}.jpg`, request.body);
-    await new ZendeskServices().uploadAttechment(fileName,fs.createReadStream(`uploads/${fileName}.jpg`)).then(async(result:any)=>{
+    await new ZendeskServices().uploadAttechment(fileName, fs.createReadStream(`uploads/${fileName}.jpg`)).then(async (result: any) => {
       response.status(200);
-          response.send({
-            success: false,
-            message: "File Uploaded sucessfully!",
-            result:result.upload
-          });
-          fs.unlinkSync(`uploads/${fileName}.jpg`);
-    }).catch((error:any)=>{
+      response.send({
+        success: false,
+        message: "File Uploaded sucessfully!",
+        result: result.upload
+      });
+      fs.unlinkSync(`uploads/${fileName}.jpg`);
+    }).catch((error: any) => {
       response.status(500);
       response.send({
         success: false,
         message: "Something went wrong, please try after sometime.",
-        error : error
+        error: error
       });
     })
-
   }
 
   // create Ticket controller 
@@ -38,29 +37,28 @@ export default class ZendeskController {
             request.body.uploadToken
           ]
         },
-        description:request.body.message,
-        raw_subject:"",
-        recipient:request.body.zendeskUserEmail,
-        subject:request.body.subject,
-        priority:"normal",
-        status:"new",
-        submitter_id:request.body.zendeskUserId,
-        type:"incident"
-
+        description: request.body.message,
+        raw_subject: "",
+        recipient: request.body.zendeskUserEmail,
+        subject: request.body.subject,
+        priority: "normal",
+        status: "new",
+        submitter_id: request.body.zendeskUserId,
+        type: "incident"
       }
 
-        await new ZendeskServices().createSupportTicket(params).then(async(result:any)=>{
-          response.status(200);
-          response.send({
-            success: false,
-            message: "Ticket created sucessfully!",
-          });
-        }).catch((error:any)=>{
+      await new ZendeskServices().createSupportTicket(params).then(async (result: any) => {
+        response.status(200);
+        response.send({
+          success: false,
+          message: "Ticket created sucessfully!",
+        });
+      }).catch((error: any) => {
         response.status(500);
         response.send({
           success: false,
           message: "Something went wrong, please try after sometime.",
-          error : error
+          error: error
         });
       })
     } catch (error) {
@@ -68,9 +66,9 @@ export default class ZendeskController {
       response.send({
         success: false,
         message: "Something went wrong, please try after sometime.",
-        error : error
+        error: error
       });
     }
-  }  
+  }
 
 }
