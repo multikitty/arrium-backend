8a2102a64dc0c608dd482e99bb64962b13d06e17
multikitty <multikitty422@gmail.com>
2022-12-30 11:58:28 +0530

    alert Notification

--- src/routes/alert.ts
+++ src/routes/alert.ts
@@ -5,25 +5,51 @@ router.use(express.json());
 import AlertController from "../Controllers/AlertController";
 import { alertValidation } from "./../validationSchema/alertValidation";
 import { validationSchema } from "./../Middlewares/validationSchema";
-import { authentication } from '../Middlewares/authentication';
+import { authentication } from "../Middlewares/authentication";
 
 //add blocks notification
-router.post("/createBlockNotification", authentication('driver'), alertValidation.createBlockNotification, validationSchema, new AlertController().insertBlockNotification);
+router.post(
+  "/createBlockNotification",
+  authentication("driver"),
+  alertValidation.createBlockNotification,
+  validationSchema,
+  new AlertController().insertBlockNotification
+);
 
 //get blocks notification
-router.get("/blockNotification", authentication('driver'), new AlertController().getNotificationList);
+router.get(
+  "/blockNotification",
+  authentication("driver"),
+  new AlertController().getNotificationList
+);
 
 //get all notification
-router.get("/allNotification/:pk", authentication('driver'), new AlertController().getAllNotificationList)
+router.get(
+  "/allNotification/:pk",
+  authentication("driver"),
+  new AlertController().getAllNotificationList
+);
 
 //update notification viewed status
-router.post("/updateViewedNotification", authentication('driver'), new AlertController().updateNotificationViewed);
+router.post(
+  "/updateViewedNotification",
+  authentication("driver"),
+  new AlertController().updateNotificationViewed
+);
 
 //update notification dismissed status
-router.post("/updateDismissedSingleNotification", authentication('driver'), new AlertController().updateDismissedDateInBlockNotification)
+router.post(
+  "/updateDismissedSingleNotification",
+  authentication("driver"),
+  new AlertController().updateDismissedDateInBlockNotification
+);
 
 // update notification dismissed all status
-router.post("/updateDismissedAllNotifications", authentication('driver'), new AlertController().updateDismissedDateInAllBlockNotification)
+router.post(
+  "/updateDismissedAllNotifications",
+  authentication("driver"),
+  new AlertController().updateDismissedDateInAllBlockNotification
+);
 
 // Testing Invoice api
 router.post("/createInvoice", new AlertController().createInvoice);
