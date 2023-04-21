7e20ae4db2f3bae26f744f757e5a4bd5ecd82b66
multikitty <multikitty422@gmail.com>
2023-04-21 17:07:23 +0530

    fix verified-email-address Api

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -12,29 +12,77 @@ import UserValidation from "../validationSchema/userValidation";
 //get User Details
 router.get("/", authentication("driver"), new UserController().getUserData);
 
-router.post("/send-otp", authentication("driver"), new UserController().sendOtpToUser);
+router.post(
+  "/send-otp",
+  authentication("driver"),
+  new UserController().sendOtpToUser
+);
 
 router.get("/list", authentication("admin"), new UserController().listAllUsers);
 // get user by sk and pk for admin
 router.get("/get", authentication("admin"), new UserController().getUserByPkSk);
 
-router.get("/flex-details/:pk", authentication("admin"), new UserController().getAmznFlexDetails);
+router.get(
+  "/flex-details/:pk",
+  authentication("admin"),
+  new UserController().getAmznFlexDetails
+);
 
-router.put("/flex-details/update", authentication("admin"), new FlexValidation().flexDetails(), validationSchema, new UserController().updateAmznFlexDetails);
+router.put(
+  "/flex-details/update",
+  authentication("admin"),
+  new FlexValidation().flexDetails(),
+  validationSchema,
+  new UserController().updateAmznFlexDetails
+);
 
-router.put("/flex-details/update-tokens", authentication("admin"), new FlexValidation().flexTokens(), validationSchema, new UserController().updateFlexTokens);
+router.put(
+  "/flex-details/update-tokens",
+  authentication("admin"),
+  new FlexValidation().flexTokens(),
+  validationSchema,
+  new UserController().updateFlexTokens
+);
 
-router.put("/update-account-info", authentication("admin"), new UserController().updateAccountInfo);
+router.put(
+  "/update-account-info",
+  authentication("admin"),
+  new UserController().updateAccountInfo
+);
 
-router.post("/approve-account", authentication("admin"), new UserController().sendAccountSetupMail);
+router.post(
+  "/approve-account",
+  authentication("admin"),
+  new UserController().sendAccountSetupMail
+);
 
-router.put("/pricing-plan", authentication("admin"), new UserController().enablePricingPlanPage);
+router.put(
+  "/pricing-plan",
+  authentication("admin"),
+  new UserController().enablePricingPlanPage
+);
 
-router.get("/list-by-role", authentication("admin"), new UserController().fetchUserByRole);
+router.get(
+  "/list-by-role",
+  authentication("admin"),
+  new UserController().fetchUserByRole
+);
 
-router.post("/add-user", authentication("admin"), new UserValidation().addUser(), validationSchema, new UserController().addUser);
+router.post(
+  "/add-user",
+  authentication("admin"),
+  new UserValidation().addUser(),
+  validationSchema,
+  new UserController().addUser
+);
 
-router.put("/update-flex-info", authentication("driver"), authSchema.updateAmazonFlexSchema, validationSchema, new UserController().updateFlexDetailsByDriver);
+router.put(
+  "/update-flex-info",
+  authentication("driver"),
+  authSchema.updateAmazonFlexSchema,
+  validationSchema,
+  new UserController().updateFlexDetailsByDriver
+);
 
 router.post(
   "/request-verify-email",
@@ -44,7 +92,7 @@ router.post(
   new UserController().sendEmailVerify
 );
 
-router.post("/verify-email", authentication("driver"), new UserController().VerifyEmail);
+router.post("/verify-email", new UserController().VerifyEmail);
 
 router.post(
   "/update-password",
@@ -69,7 +117,6 @@ router.post(
   validationSchema,
   new UserController().updatephoneNumber
 );
-
 
 router.post(
   "/update-email",
