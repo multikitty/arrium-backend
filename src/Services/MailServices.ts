524d1edeb0856079d1364e1c2293d497f64786dd
multikitty <multikitty422@gmail.com>
2023-04-21 13:17:06 +0530

    change email templates ui

--- src/Services/MailServices.ts
+++ src/Services/MailServices.ts
@@ -1,90 +1,107 @@
 import SqsQueueServices from "./SqsQueueServices";
 export default class MailServices {
-  
   async newUserSignUpMail(data: any) {
     const msgData = {
-      type : "mail",
-      data : {
-        to : 'dipanshu.jaiswal@scaleupally.io',
-        from : 'notification@arrium.io',
-        replyTo : [],
-        subject : `Approve new user account.`,
-        message : `
-          Dear Admin,
-          A new user has provided their Amazon Flex login details at the sign up step, and are at the 'holding page' step, awaiting account setup to be completed.
-          <br/>Full Name: ${data.firstname} ${data.lastname}
-          <br/>Email Address: ${data.email}
-          `
-      }
-    }
+      type: "mail",
+      data: {
+        to: "info@braincuber.com",
+        from: "notification@arrium.io",
+        replyTo: [],
+        subject: `New Account Setup`,
+        message: `
+            <p>Hi Admin,</p>
+            <p>A new user has provided their Amazon Flex login details at the sign-up step, and are at the 'holding page' step, awaiting account setup to be completed.</p>
+            <p>Find the customer using the below details, to configure their account</p>
+            <p>Full Name: ${data.firstname} ${data.lastname}</p>
+            <p>Email Address:${data.email} </p>
+            <p>- the Support Team at Arrium</p>
+          `,
+      },
+    };
     return new SqsQueueServices().sendMessageInNotificationQueue(msgData);
   }
 
   async accountSetupMail(data: any) {
     const msgData = {
-      type : "mail",
-      data : {
-        to : data.userEmail,
-        from : 'notification@arrium.io',
-        replyTo : [],
-        subject : `Account Configured`,
-        message : `
-            Dear ${data.userName}, <br/>
-            Your account has been configured and you can begin using Arrium to start booking your blocks with Amazon Flex.
-          `
-      }
-    }
+      type: "mail",
+      data: {
+        to: data.userEmail,
+        from: "support@arrium.io",
+        replyTo: "support@arrium.io",
+        subject: `Arrium account is ready`,
+        message: `
+            <p>Hi ${data.userName}</p>
+            <p>Your account is ready to start accepting offers for Amazon Flex.</p>
+            <p>Click <a href="https://www.arrium.io/">here<a/> to login</p>
+            <p>Or, you can copy and paste this link into a web browser: <a href="https://www.arrium.io/">insert link here<a/></p>
+            <p>- the Support Team at Arrium</p>
+          `,
+      },
+    };
     return new SqsQueueServices().sendMessageInNotificationQueue(msgData);
   }
 
   async sendMailEmailVerification(data: any) {
     const msgData = {
-      type : "mail",
-      data : {
-        to : data.email,
-        from : 'noreply@arrium.io',
-        replyTo : [],
-        subject : `Email Verification`,
-        message : `
-            <p>Please follow the below link to verify your email address</p><br /><p>https://arrium.io/signupEmailVerify/?token=${data.token}</p>
-          `
-      }
-    }
+      type: "mail",
+      data: {
+        to: data.email,
+        from: "support@arrium.io",
+        replyTo: "support@arrium.io",
+        subject: `Verify your email address`,
+        message: `
+            <p>Hi</p>
+            <p>We need to verify your email address to keep your account safe.</p>
+            <p>Click <a href="https://arrium.io/signupEmailVerify/?token=${data.token}">here<a/> to verify your email address.</p>
+            <p>Or, you can copy and paste this link into a web browser: <a href="https://arrium.io/signupEmailVerify">insert link here<a/></p>
+            <p>- the Support Team at Arrium</p>
+          `,
+      },
+    };
     return new SqsQueueServices().sendMessageInNotificationQueue(msgData);
   }
 
   async sendMailForgotPassword(data: any) {
     const msgData = {
-      type : "mail",
-      data : {
-        to : data.email,
-        from : 'noreply@arrium.io',
-        replyTo : [],
-        subject : "Reset Password",
-        message :`
-            <p>Please follow the below link to reset your password</p><br /><p>https://arrium.io/reset-password/?token=${data.token}</p></br>
-          `
-      }
-    }
+      type: "mail",
+      data: {
+        to: data.email,
+        from: "support@arrium.io",
+        replyTo: "support@arrium.io",
+        subject: "Password Reset",
+        message: `
+            <p>Hi</p>
+            <p>We’ve received a request to reset your password.</p>
+            <p>Click <a href="https://arrium.io/reset-password/?token=${data.token}">here<a/> to reset your password.</p>
+            <p>Or, you can copy and paste this link into a web browser: <a href="https://arrium.io/reset-password">insert link here<a/></p>
+            <p>If you did not initiate this request, please contact us immediately at <a href="mailto:support@arrium.io">support@arrium.io<a/></p>
+            <p>- the Support Team at Arrium</p>
+          `,
+      },
+    };
     return new SqsQueueServices().sendMessageInNotificationQueue(msgData);
   }
 
   /**
    * sendBlockAcceptedMail
    */
-  public async sendBlockAcceptedMail(data : any) {
+  public async sendBlockAcceptedMail(data: any) {
     const msgData = {
-      type : "mail",
-      data : {
-        to : data.user.userEmail,
-        from : 'notification@arrium.io',
-        replyTo : [],
-        subject : "Block(s) Accepted",
-        message : `Dear ${data.user.userName},<br />
-        The following block(s) have been accepted:<br/><br/>
-        ${data.blockInfo}`
-      }
-    }
+      type: "mail",
+      data: {
+        to: data.user.userEmail,
+        from: "notification@arrium.io",
+        replyTo: [],
+        subject: "Offer(s) Accepted",
+        message: `
+          <p>Hi ${data.user.userName}</p>
+          <p>Arrium has accepted the following offer(s) for you.</p>
+          ${data.blockInfo}
+          <p>Drive safely.</p>
+          <p>- the Support Team at Arrium</p>
+        `,
+      },
+    };
     return await new SqsQueueServices().sendMessageInNotificationQueue(msgData);
   }
 }
